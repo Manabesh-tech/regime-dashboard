371b5ffe83883cd2186cc2a4cf92bb92a76878a7
freedom.chen <freedom.chen@nfex.io>
2025-08-15 09:56:06 +0800

    fix

--- table.sql
+++ table.sql
@@ -20,6 +20,16 @@ select all_bid,all_ask  from oracle_exchange_price_partition_v1_20250526 where p
 
 //query exchange_fee
 select * from oracle_exchange_fee;
+//query  surf buffer rate
+select *from trade_pair_risk_history tprh where pair_name='PUMP/USDT' and created_at >='2025-07-24 5:15:00.000 +0800' and created_at <='2025-07-24 14:02:29.011 +0800';
+//query surf volatility history
+select *from  trade_pair_volatility_log where pair_name='PUMP/USDT' and created_at >='2025-07-24 5:16:00.000 +0800' and created_at <='2025-07-24 14:02:29.011 +0800';
+//query rollbit buffer rate
+select *from rollbit_pair_config where pair_name='1000PUMP/USDT' and created_at >='2025-08-15 01:18:00.000 +0800' and created_at <='2025-07-24 01:20:00.011 +0800';
+
+
+
+
 
 
 
