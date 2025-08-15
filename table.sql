6997e232a837ca68fb006065279eb41296d03f06
freedom.chen <freedom.chen@nfex.io>
2025-08-15 09:57:55 +0800

    fix

--- table.sql
+++ table.sql
@@ -1,3 +1,13 @@
+
+uat_db_params = {
+    'host': 'aws-jp-tk-surf-pg-public.cluster-csteuf9lw8dv.ap-northeast-1.rds.amazonaws.com',
+    'port': 5432,
+    'database': 'report_dev',  # Different database
+    'user': 'public_rw',     # Different user
+    'password': 'aTJ92^kl04hllk'  # Different password
+}
+
+
 //query surf all trade  pairs
 select *from trade_pool_pairs where status in (1,2);
 
@@ -21,35 +31,9 @@ select all_bid,all_ask  from oracle_exchange_price_partition_v1_20250526 where p
 //query exchange_fee
 select * from oracle_exchange_fee;
 //query  surf buffer rate
-select *from trade_pair_risk_history tprh where pair_name='PUMP/USDT' and created_at >='2025-07-24 5:15:00.000 +0800' and created_at <='2025-07-24 14:02:29.011 +0800';
+select *from trade_pair_risk_history tprh where pair_name='PUMP/USDT' and created_at >='2025-07-24 5:15:00.000 +0800' and created_at <='2025-07-24 14:02:29.011 +0800' ORDER BY created_at DESC;
 //query surf volatility history
-select *from  trade_pair_volatility_log where pair_name='PUMP/USDT' and created_at >='2025-07-24 5:16:00.000 +0800' and created_at <='2025-07-24 14:02:29.011 +0800';
+select *from  trade_pair_volatility_log where pair_name='PUMP/USDT' and created_at >='2025-07-24 5:16:00.000 +0800' and created_at <='2025-07-24 14:02:29.011 +0800' ORDER BY created_at DESC;
 //query rollbit buffer rate
-select *from rollbit_pair_config where pair_name='1000PUMP/USDT' and created_at >='2025-08-15 01:18:00.000 +0800' and created_at <='2025-07-24 01:20:00.011 +0800';
+select *from rollbit_pair_config where pair_name='1000PUMP/USDT' and created_at >='2025-08-15 01:18:00.000 +0800' and created_at <='2025-07-24 01:20:00.011 +0800' ORDER BY created_at DESC;
 
-
-
-
-
-
-
-uat_db_params = {
-    'host': 'aws-jp-tk-surf-pg-public.cluster-csteuf9lw8dv.ap-northeast-1.rds.amazonaws.com',
-    'port': 5432,
-    'database': 'report_dev',  # Different database
-    'user': 'public_rw',     # Different user
-    'password': 'aTJ92^kl04hllk'  # Different password
-}
-
-//query rollbit_pair_config
-SELECT bust_buffer, created_at
-FROM rollbit_pair_config
-WHERE pair_name='1000PUMP/USDT' and created_at >= '2025-07-18 19:34:00'
-  AND created_at <= '2025-07-18 19:36:00'
-ORDER BY created_at DESC;
-//query surf buffer_rate config
-SELECT buffer_rate,created_at
-FROM trade_pair_risk_history
-WHERE pair_name='PUMP/USDT' and created_at >= '2025-07-18 19:34:00'
-  AND created_at <= '2025-07-18 19:36:00'
-ORDER BY created_at DESC;
